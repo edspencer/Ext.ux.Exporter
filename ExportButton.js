d6ae51a2b060225128c6e1e6e892d1202737ed8f
Ed Spencer <edward@midlandswebdesign.com>
2009-08-14 00:55:28 +0100

    Added ExportButton

--- ExportButton.js
+++ ExportButton.js
@@ -1,18 +1,77 @@
 /**
  * @class Ext.ux.ExportButton
  * @extends Ext.Button
+ * @author Nige White, with modifications from Ed Spencer
  * Specialised Button class that allows downloading of data via data: urls.
- * Internally, this is just a link
+ * Internally, this is just a link.
+ * Pass it either an Ext.Component subclass with a 'store' property, or just a store:
+ * new Ext.ux.ExportButton({component: someGrid});
+ * new Ext.ux.ExportButton({store: someStore});
+ * @cfg {Ext.Component} component The component the store is bound to
+ * @cfg {Ext.data.Store} store The store to export (alternatively, pass a component with a store property)
  */
 Ext.ux.ExportButton = Ext.extend(Ext.Button, {
-
-  initComponent: function() {
-    Ext.applyIf(this, {
-      
-    });
+  constructor: function(config) {
+    config = config || {};
     
-    Ext.ux.ExportButton.superclass.initComponent.apply(this, arguments);
-  }
+    if (config.store == undefined && config.component != undefined) {
+      Ext.applyIf(config, {
+        store: config.component.store
+      });
+    } else {
+      Ext.applyIf(config, {
+        component: {
+          store: config.store
+        }
+      });      
+    }
+    
+    Ext.ux.ExportButton.superclass.constructor.call(this, config);
+    
+    if (this.store && Ext.isFunction(this.store.on)) {
+      this.store.on('load', function(store, records) {
+        var setLink = function() {
+          this.getEl().child('a', true).href = 'data:application/vnd.ms-excel;base64,' + Ext.ux.Exporter.exportGrid(this.component);
+        };
+        
+        if (this.el) {
+          setLink.call(this);
+        } else {
+          this.on('render', setLink, this);
+        }
+      }, this);
+    }
+  },
+  
+  template: new Ext.Template(
+    '<table border="0" cellpadding="0" cellspacing="0" class="x-btn-wrap"><tbody><tr>',
+    '<td class="x-btn-left"><i> </i></td><td class="x-btn-center"><a class="x-btn-text" href="{1}" target="{2}">{0}</a></td><td class="x-btn-right"><i> </i></td>',
+    "</tr></tbody></table>"),
+
+    onRender:   function(ct, position){
+      var btn, targs = [this.text || ' ', this.href, this.target || "_self"];
+      if (position){
+        btn = this.template.insertBefore(position, targs, true);
+      }else{
+        btn = this.template.append(ct, targs, true);
+      }
+      var btnEl = btn.child("a:first");
+      btnEl.on('focus', this.onFocus, this);
+      btnEl.on('blur', this.onBlur, this);
+
+      this.initButtonEl(btn, btnEl);
+      Ext.ButtonToggleMgr.register(this);
+    },
+
+    onClick : function(e){
+      if (e.button != 0) return;
+      
+      if (!this.disabled){
+        this.fireEvent("click", this, e);
+        
+        if (this.handler) this.handler.call(this.scope || this, this, e);
+      }
+    }
 });
 
 Ext.reg('exportbutton', Ext.ux.ExportButton);
